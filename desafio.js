a052f3f06b67e9fac8f21ae5d382d146b39f6ab5
Luzamir Medina <medinaluzamir@gmail.com>
2022-09-08 17:16:00 -0500

    actualizacion

--- desafio.js
+++ desafio.js
@@ -1,10 +1,10 @@
-console.log('correcto');
+//console.log('correcto');
 
-document.querySelector('#boton').addEventListener('click', traerDatos());
+document.querySelector('#boton').addEventListener('click', traerDatos);
 
 function traerDatos(){
    
-  console,log('dentro de la funcion');
+  //console,log('dentro de la funcion');
 
   const xhttp = new XMLHttpRequest();
 
@@ -17,7 +17,25 @@ function traerDatos(){
 
     if(this.readyState == 4 && this.status == 200){
 
-      console.log(this.responseText);
+      //console.log(this.responseText);
+      let datos = JSON.parse(this.responseText);
+      //console.log(datos);
+      let respuesta =document.querySelector('#respuesta');
+      respuesta.innerHTML = ' ';
+
+      for(let item of datos){
+       // console.log(item.nombre);
+       respuesta.innerHTML += `
+            <tr>
+               <td>${item.identificación}</td>
+               <td>${item.nombre}</td> 
+               <td>${item.apellido}</td>
+               <td>${item.salario}</td> 
+               <td>${item.área}</td> 
+            </tr>
+       `
+      }
+
     }
   }
 
